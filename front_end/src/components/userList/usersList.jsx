8298172e356eb1ecaa84b8ce8e6b0937ec5ff35f
Nicolas DECRESSAC <101709161+Nilyss@users.noreply.github.com>
2022-08-23 14:36:03 +0200

    Change usersList to Administrators list
    review the component utilities

--- front_end/src/components/userList/usersList.jsx
+++ front_end/src/components/userList/usersList.jsx
@@ -1,5 +1,5 @@
 // libraries
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
@@ -10,15 +10,18 @@ import { PostContext } from '../../context'
 import './_usersList.scss'
 
 export default function UsersList() {
-  const { isLoading, users } = useContext(PostContext)
+  const { isLoading, users, user } = useContext(PostContext)
 
   return (
     <>
       <div className="userListContainer">
-        <h3 className="userListContainer__Title">Users list</h3>
+        <h3 className="userListContainer__Title">Administrators</h3>
         <ul className="userListContainer__ul">
           {users &&
-            users.map((user, index) => {
+            users.map((userMap, index) => {
+              const isAdmin = userMap.isAdmin === 1
+              const isOnline = userMap._id === user._id
+
               return (
                 <li className="userListContainer__ul__list" key={index}>
                   {isLoading ? (
@@ -27,18 +30,25 @@ export default function UsersList() {
                       className="fa-spin fa-2x postFlow"
                     />
                   ) : (
-                    <>
-                      <figure>
-                        <img
-                          className="userListContainer__ul__list__img"
-                          src={user.profilePicture}
-                          alt="user profile"
-                        />
-                      </figure>
-                      <h4 className="userListContainer__ul__list__name">
-                        {user.firstName} {user.lastName}
-                      </h4>
-                    </>
+                    isAdmin && (
+                      <>
+                        {isOnline ? (
+                          <div className="online" title="is online"></div>
+                        ) : (
+                          <div className="offline" title="is offline"></div>
+                        )}
+                        <figure>
+                          <img
+                            className="userListContainer__ul__list__img"
+                            src={userMap.profilePicture}
+                            alt="user profile"
+                          />
+                        </figure>
+                        <h4 className="userListContainer__ul__list__name">
+                          {userMap.firstName} {userMap.lastName}
+                        </h4>
+                      </>
+                    )
                   )}
                 </li>
               )
